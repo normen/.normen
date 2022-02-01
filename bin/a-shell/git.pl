faee88541228245564c163de4f4ecd3417f1c576
normen <normen667@gmail.com>
2022-02-01 14:11:11 +0100

    clone cred 👯

--- bin/a-shell/git.pl
+++ bin/a-shell/git.pl
@@ -4,19 +4,21 @@ use feature "say";
 my $args=join(" ", @ARGV);
 my $key;
 my $host;
-if(open my $fh, '<', glob '.git/config'){
+if(@ARGV > 1 && $ARGV[0] eq "clone") {
+  $host = $ARGV[1];
+} elsif (open my $fh, '<', glob '.git/config') {
   my $content = do {local $/;<$fh>};
   close $fh;
   ($host) = $content =~ m/^\s*url\s*=\s*[^\s:]*:\/\/([^\s\/\$]*)/gm;
 }
-if($host && open my $fh, '<', glob '~/Documents/.git-credentials'){
+if ($host && open my $fh, '<', glob '~/Documents/.git-credentials') {
   my $content = do {local $/;<$fh>};
   close $fh;
   ($key) = $content =~ m/^[^\s:]*:[^\s:]*:([^\@]*)\@$host/gm;
 }
-if($key){
+if ($key) {
   say "Found key in .git-credential";
   exit system("echo $key | lg2 $args");
-} else{
+} else {
   exit system("lg2 $args");
 }
