0265ccce123ce99ff01b4fbc7c076ece59adacd1
normen <normen667@gmail.com>
2022-10-22 17:37:22 +0200

    arscherl 🍑

--- bin/a-shell/git.pl
+++ bin/a-shell/git.pl
@@ -18,7 +18,7 @@ if ($host && open my $fh, '<', glob '~/Documents/.git-credentials') {
 }
 if ($key) {
   say "Found key in .git-credential";
-  exit system("echo $key | lg2 $args");
+  exec("echo $key | lg2 $args");
 } else {
-  exit system("lg2 $args");
+  exec("lg2 $args");
 }
