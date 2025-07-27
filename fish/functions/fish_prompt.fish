2748fe7b9fd4a557ada13700e01917229458fb67
Normen <normen667@gmail.com>
2025-07-27 20:44:41 +0200

    prompt 🛣️

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,138 +1,55 @@
 function fish_prompt
-        # This prompt shows:
-        # - green lines if the last return command is OK, red otherwise
-        # - your user name, in red if root or yellow otherwise
-        # - your hostname, in cyan if ssh or blue otherwise
-        # - the current path (with prompt_pwd)
-        # - date +%X
-        # - the current virtual environment, if any
-        # - the current git status, if any, with fish_git_prompt
-        # - the current battery state, if any, and if your power cable is unplugged, and if you have "acpi"
-        # - current background jobs, if any
+        set -l last_status $status
     
-        # It goes from:
-        # ┬─[nim@Hattori:~]─[11:39:00]
-        # ╰─>$ echo here
+        set -l normal (set_color normal)
+        set -l usercolor (set_color $fish_color_user)
     
-        # To:
-        # ┬─[nim@Hattori:~/w/dashboard]─[11:37:14]─[V:django20]─[G:master↑1|●1✚1…1]─[B:85%, 05:41:42 remaining]
-        # │ 2    15054    0%    arrêtée    sleep 100000
-        # │ 1    15048    0%    arrêtée    sleep 100000
-        # ╰─>$ echo there
+        set -l delim \U25BA
+        # If we don't have unicode use a simpler delimiter
+        string match -qi "*.utf-8" -- $LANG $LC_CTYPE $LC_ALL; or set delim ">"
     
-        set -l retc red
-        test $status = 0; and set retc green
+        fish_is_root_user; and set delim "#"
     
-        set -q __fish_git_prompt_showupstream
-        or set -g __fish_git_prompt_showupstream auto
-    
-        function _nim_prompt_wrapper
-                set retc $argv[1]
-                set -l field_name $argv[2]
-                set -l field_value $argv[3]
+        set -l cwd (set_color $fish_color_cwd)
+        if command -sq cksum
+                # randomized cwd color
+                # We hash the physical PWD and turn that into a color. That means directories (usually) get different colors,
+                # but every directory always gets the same color. It's deterministic.
+                # We use cksum because 1. it's fast, 2. it's in POSIX, so it should be available everywhere.
+                set -l shas (pwd -P | cksum | string split -f1 ' ' | math --base=hex | string sub -s 3 | string pad -c 0 -w 6 | string match -ra ..)
+                set -l col 0x$shas[1..3]
         
-                set_color normal
-                set_color $retc
-                echo -n '─'
-                set_color -o green
-                echo -n '['
-                set_color normal
-                test -n $field_name
-                and echo -n $field_name:
-                set_color $retc
-                echo -n $field_value
-                set_color -o green
-                echo -n ']'
-        end
-    
-        set_color $retc
-        echo -n '┬─'
-        set_color -o green
-        echo -n [
-    
-        if functions -q fish_is_root_user; and fish_is_root_user
-                set_color -o red
-        else
-                set_color -o yellow
-        end
-    
-        echo -n $USER
-        set_color -o white
-        echo -n @
-    
-        if test -z "$SSH_CLIENT"
-                set_color -o blue
-        else
-                set_color -o cyan
-        end
-    
-        echo -n (prompt_hostname)
-        set_color -o white
-        echo -n :(prompt_pwd)
-        set_color -o green
-        echo -n ']'
-    
-        # Date
-        _nim_prompt_wrapper $retc '' (date +%X)
-    
-        # Vi-mode
-        # The default mode prompt would be prefixed, which ruins our alignment.
-        function fish_mode_prompt
-        end
-    
-        if test "$fish_key_bindings" = fish_vi_key_bindings
-                or test "$fish_key_bindings" = fish_hybrid_key_bindings
-                set -l mode
-                switch $fish_bind_mode
-                        case default
-                                set mode (set_color --bold red)N
-                        case insert
-                                set mode (set_color --bold green)I
-                        case replace_one
-                                set mode (set_color --bold green)R
-                                echo '[R]'
-                        case replace
-                                set mode (set_color --bold cyan)R
-                        case visual
-                                set mode (set_color --bold magenta)V
+                # If the (simplified idea of) luminance is below 120 (out of 255), add some more.
+                # (this runs at most twice because we add 60)
+                while test (math 0.2126 x $col[1] + 0.7152 x $col[2] + 0.0722 x $col[3]) -lt 120
+                        set col[1] (math --base=hex "min(255, $col[1] + 60)")
+                        set col[2] (math --base=hex "min(255, $col[2] + 60)")
+                        set col[3] (math --base=hex "min(255, $col[3] + 60)")
                 end
-                set mode $mode(set_color normal)
-                _nim_prompt_wrapper $retc '' $mode
+                set -l col (string replace 0x '' $col | string pad -c 0 -w 2 | string join "")
+        
+                set cwd (set_color $col)
         end
     
-        # Virtual Environment
-        set -q VIRTUAL_ENV_DISABLE_PROMPT
-        or set -g VIRTUAL_ENV_DISABLE_PROMPT true
-        set -q VIRTUAL_ENV
-        and _nim_prompt_wrapper $retc V (path basename "$VIRTUAL_ENV")
+        # Prompt status only if it's not 0
+        set -l prompt_status
+        test $last_status -ne 0; and set prompt_status (set_color $fish_color_status)"[$last_status]$normal"
     
-        # git
-        set -l prompt_git (fish_git_prompt '%s')
-        test -n "$prompt_git"
-        and _nim_prompt_wrapper $retc G $prompt_git
-    
-        # Battery status
-        type -q acpi
-        and acpi -a 2>/dev/null | string match -rq off
-        and _nim_prompt_wrapper $retc B (acpi -b | cut -d' ' -f 4-)
-    
-        # New line
-        echo
-    
-        # Background jobs
-        set_color normal
-    
-        for job in (jobs)
-                set_color $retc
-                echo -n '│ '
-                set_color brown
-                echo $job
+        # Only show host if in SSH or container
+        # Store this in a global variable because it's slow and unchanging
+        if not set -q prompt_host
+                set -g prompt_host ""
+                if set -q SSH_TTY
+                        or begin
+                                command -sq systemd-detect-virt
+                                and systemd-detect-virt -q
+                        end
+                        set prompt_host $usercolor$USER$normal@(set_color $fish_color_host)$hostname$normal":"
+                end
         end
     
-        set_color normal
-        set_color $retc
-        echo -n '╰─>'
-        set_color -o red
-        echo -n '$ '
-        set_color normal
+        # Shorten pwd if prompt is too long
+        set -l pwd (prompt_pwd)
+    
+        echo -n -s $prompt_host $cwd $pwd $normal $prompt_status $delim " "
 end
