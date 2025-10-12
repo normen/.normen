08725c57275abc91c16af3a7f8e0d7c88ba400fd
Normen <normen667@gmail.com>
2025-10-12 16:14:02 +0200

    fishy 🐟

--- fish/functions/_fish_ai_codify_or_explain.fish
+++ fish/functions/_fish_ai_codify_or_explain.fish
@@ -1,18 +1,18 @@
 #!/usr/bin/env fish
 
 function _fish_ai_codify_or_explain --description "Transform a command into a comment and vice versa using AI."
-    set input (commandline --current-buffer)
-
-    _fish_ai_show_progress_indicator
+    set -f input (commandline --current-buffer)
 
     if test -z "$input"
         return
     end
 
+    _fish_ai_show_progress_indicator
+
     if test (string sub --length 2 "$input") = "# "
-        set output (_fish_ai_codify "$input")
+        set -f output (_fish_ai_codify "$input")
     else
-        set output (_fish_ai_explain "$input")
+        set -f output (_fish_ai_explain "$input")
     end
 
     commandline --replace "$output"
